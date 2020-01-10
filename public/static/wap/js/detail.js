b74bd0fad709aae7056520e63e281964cbabfdf0
qinjianfei <qc_406eea3bca2c47ceac78d156a1b94982@git.code.tencent.com>
2020-01-10 22:31:34 +0800

    编写 文章详情2

--- public/static/wap/js/detail.js
+++ public/static/wap/js/detail.js
@@ -1,12 +1,26 @@
 layui.use('layer', () =>{
+
   // 获取jquery
   let $ = layui.$
   $('#btn-comment').on('click', function(e) {
-    console.log(e);
+    // console.log(e);
     let that = $(this)
     // 切换评论状态
     $('.detail-comment-box').toggle()
     
+  })
+
+  $('#id-edit').on('focus', function(e) {
+    console.log('focus', e);
+    $('.detail-comment-input').css({
+      'border-color': '#5FB878'
+    })
+  })
+  $('#id-edit').on('blur', function(e) {
+    console.log('blur', e);
+    $('.detail-comment-input').css({
+      'border-color': '#ccc'
+    })
   })
   
 });
